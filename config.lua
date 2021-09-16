fc12ca5fc371c28b6f7c85b0543b86582b9c30b9
Julian Tao <julian.tao@asu.edu>
2021-09-16 12:08:00 -0700

    remove vimwiki pandoc filetype

--- config.lua
+++ config.lua
@@ -52,7 +52,7 @@ lvim.plugins = {
 			vim.g.vimwiki_commentstring = "<!--%s-->"
 			vim.g.vimwiki_folding = ""
 			vim.g.vimwiki_markdown_link_ext = 1
-			vim.g.vimwiki_filetypes = { "Rmd", "pandoc" }
+			vim.g.vimwiki_filetypes = { "rmd" }
 			vim.g.vimwiki_key_mappings = { table_mappings = 0, lists_return = 0 }
 		end,
 	},
