025c76329d4b2027a0b0a861b6a3d79f9a4f69b3
Julian Tao <julian.tao@asu.edu>
2021-11-03 08:58:03 -0700

    Input image name

--- config.lua
+++ config.lua
@@ -73,6 +73,12 @@ lvim.plugins = {
 		config = function()
 			require("clipboard-image").setup({
 				default = {
+					img_name = function()
+						vim.fn.inputsave()
+						local name = vim.fn.input("Name: ")
+						vim.fn.inputrestore()
+						return name
+					end,
 					affix = "![](%s)",
 				},
 			})
