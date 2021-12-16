a09c83c22a9e15eafd9ec257f5c819e4eaf950f4
Julian Tao <julian.tao@asu.edu>
2021-12-16 09:45:42 -0700

    Do not allow vimtex to auto compile tex file when opening a file

--- config.lua
+++ config.lua
@@ -112,7 +112,6 @@ lvim.plugins = {
         augroup vimtex_event_1
             au!
             au User VimtexEventQuit     call vimtex#compiler#clean(0)
-            au User VimtexEventInitPost call vimtex#compiler#compile()
         augroup END
         ]],
 				false
