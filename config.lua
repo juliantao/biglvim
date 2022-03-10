796d03df8237dd71bc44ec6083b1cfecd5b1b996
JulianTao <julian.tao@asu.edu>
2022-03-09 22:35:57 -0700

    Update snip for diary yml header

--- config.lua
+++ config.lua
@@ -193,7 +193,7 @@ lvim.plugins = {
 	{
 		"dkarter/bullets.vim",
 		config = function()
-			-- vim.g.bullets_mapping_leader = "<SPACE>"
+			vim.g.bullets_mapping_leader = "<SPACE>"
 			vim.g.bullets_enabled_file_types = {
 				"markdown",
 				"quarto",
