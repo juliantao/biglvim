2272ebb4fb07bb3858cab1e5b992a6c80fe37f1f
JulianTao <julian.tao@asu.edu>
2022-03-11 00:12:53 -0700

    Fix typo

--- config.lua
+++ config.lua
@@ -196,7 +196,7 @@ lvim.plugins = {
 			vim.g.bullets_mapping_leader = "<SPACE>"
 			vim.g.bullets_enabled_file_types = {
 				"markdown",
-				"quarto",
+				-- "quarto",
 				"rmarkdown",
 				"rmd",
 				"pandoc",
