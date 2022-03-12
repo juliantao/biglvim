6c81b2d2c60906d1cf6fa622d24326c25785288d
JulianTao <julian.tao@asu.edu>
2022-03-11 23:08:01 -0700

    Set concellevel to 0 for vimwiki

--- config.lua
+++ config.lua
@@ -116,6 +116,7 @@ lvim.plugins = {
 			}
 			vim.g.vimwiki_commentstring = "<!--%s-->"
 			vim.g.vimwiki_folding = ""
+			vim.g.vimwiki_conceallevel = 0
 			vim.g.vimwiki_markdown_link_ext = 1
 			vim.g.vimwiki_filetypes = { "quarto" }
 			vim.g.vimwiki_key_mappings = { table_mappings = 0, lists_return = 0 }
