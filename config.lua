229032a95cd91f52f9454b3478e08b5940bdf796
Julian Tao <julian.tao@asu.edu>
2022-04-26 10:01:18 -0700

    Add ltex lsp

--- config.lua
+++ config.lua
@@ -289,6 +289,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 lvim.lsp.diagnostics.virtual_text = false
 require("lspconfig").r_language_server.setup({})
 require("lspconfig").dotls.setup({})
+-- require("lspconfig").ltex.setup({ filetypes = { "rmd", "bib", "tex", "quarto", "markdown" } })
 -- require("lspconfig").grammarly.setup({ filetypes = { "rmd", "tex", "qmd", "markdown", "vimwiki" } })
 
 -- Snippet
